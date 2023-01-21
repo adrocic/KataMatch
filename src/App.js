5fceb51ff8575cff5f2332599c1e6bed2eee4278
Anel Drocic <anel.drocic@gmail.com>
2023-01-21 02:46:21 -0600

    [feature/input-screen] - added basic input screen boilerplate with form, submission handling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
+import { useState, useEffect, useCallback } from 'react';
+import { OpenAI } from './openAI';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [matchData, setMatchData] = useState();
+  const [aiResponse, setAiResponse] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await OpenAI.gatherMyMatchData()
+      setMatchData(prev => [...prev, response])
+    }
+    fetchData();
+  }, [])
+
+  // Handle the form submission
+  const handleSubmit = useCallback(async (event) => {
+    event.preventDefault();
+    const response = await OpenAI.analyzeData(input, matchData)
+    setAiResponse(() => response)
+  }, [input, matchData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Question:
+        <input
+          type="text"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+      </label>
+      <button type="submit">Submit</button>
+      <p>AI RESPONSE: {aiResponse}</p>
+    </form>
   );
 }
 
